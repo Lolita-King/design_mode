5f227656aec9f3d5f2df8c3fed89fbad384e1751
lolita <1229106519@qq.com>
2023-05-25 15:59:47 +0800

    [单例模式][懒汉模式][在调用时才进行初始化 缺点：增加了同步开销]

--- app/src/main/java/com/example/designmode/singleton/Singleton.java
+++ app/src/main/java/com/example/designmode/singleton/Singleton.java
@@ -1,4 +1,13 @@
 package com.example.designmode.singleton;
 
 public class Singleton {
+    private static Singleton instance;
+    private Singleton(){}
+    //多线程同步，每次都需要进行同步
+    public static synchronized Singleton getInstance(){
+        if (instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
 }
