f1636fd518169defc89cdf4d650839487e9fe158
lolita <1229106519@qq.com>
2023-05-25 16:20:00 +0800

    [单例模式][DCL Double Check Lock][两次检测、优化同步开销]

--- app/src/main/java/com/example/designmode/singleton/Singleton.java
+++ app/src/main/java/com/example/designmode/singleton/Singleton.java
@@ -4,9 +4,15 @@ public class Singleton {
     private static Singleton instance;
     private Singleton(){}
     //多线程同步，每次都需要进行同步
-    public static synchronized Singleton getInstance(){
+    public static Singleton getInstance(){
+        //避免不必要的同步
         if (instance == null){
-            instance = new Singleton();
+            synchronized (Singleton.class){
+                //防止多线程冲突
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
         }
         return instance;
     }
