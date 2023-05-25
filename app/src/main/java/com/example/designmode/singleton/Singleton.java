3b77eba20e2cf8a2d4bbdfee649a4de50caf088a
lolita <1229106519@qq.com>
2023-05-25 16:43:06 +0800

    [单例模式][内部静态类][推荐的方式]

--- app/src/main/java/com/example/designmode/singleton/Singleton.java
+++ app/src/main/java/com/example/designmode/singleton/Singleton.java
@@ -1,19 +1,17 @@
 package com.example.designmode.singleton;
 
 public class Singleton {
-    private static Singleton instance;
     private Singleton(){}
     //多线程同步，每次都需要进行同步
     public static Singleton getInstance(){
-        //避免不必要的同步
-        if (instance == null){
-            synchronized (Singleton.class){
-                //防止多线程冲突
-                if (instance == null) {
-                    instance = new Singleton();
-                }
-            }
-        }
-        return instance;
+
+        return SingletonHolder.instance;
+    }
+
+    /**
+     * 静态内部类
+     */
+    private static class SingletonHolder{
+        private static final Singleton instance = new Singleton();
     }
 }
